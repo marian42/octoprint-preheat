3e78cb750051a1ce083207d0fb51bfa1dea25de3
Marian <mail@marian42.de>
2017-11-18 20:04:20 +0000

    Improve button layout

--- octoprint_preheat/static/js/preheat.js
+++ octoprint_preheat/static/js/preheat.js
@@ -2,6 +2,7 @@ $(function() {
 	function PreheatViewModel(parameters) {
 		var self = this;
 		self.settings = undefined;
+		self.btnPreheat = undefined;
 		
         self.allSettings = parameters[0];
         self.loginState = parameters[1];
@@ -37,20 +38,33 @@ $(function() {
 			});
 		};
 		
-		self.enablePreheat = function() {
-			return true;
+		
+		self.btnPreheatClick = function() {
+			self.preheat();
+		}
+		
+		self.initializeButton = function() {
+			var buttonContainer = $('#job_print')[0].parentElement;
+			buttonContainer.children[0].style.width = "100%";
+			buttonContainer.children[0].style.marginBottom = "10px";
+			buttonContainer.children[1].style.marginLeft = "0";
+			
+			self.btnPreheat = document.createElement("button");
+			self.btnPreheat.classList.add("btn");
+			self.btnPreheat.classList.add("span4");
+			self.btnPreheat.title = "Preheats the nozzle for the loaded gcode file.";
+			self.btnPreheat.innerText = "Preheat";
+			self.btnPreheat.addEventListener("click", self.btnPreheatClick);
+			buttonContainer.appendChild(self.btnPreheat);
 		};
 
-		self.visibleTest = function() {
-			return self.loginState.isUser() && self.printerState.isOperational();
-		};
+		self.initializeButton();
 	}
-
 	
 	// view model class, parameters for constructor, container to bind to
 	OCTOPRINT_VIEWMODELS.push([
 		PreheatViewModel,
 		["settingsViewModel", "loginStateViewModel", "printerStateViewModel"],
-		["#sidebar_plugin_preheat"]
+		[]
 	]);
 });
