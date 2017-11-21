b6c94a3d30cdb10c66604d599439a971814873ec
Marian <mail@marian42.de>
2017-11-21 17:00:25 +0000

    Rewrite gcode parsing to handle more keywords, multiple tools and bed heating references #2 fixes #1

--- octoprint_preheat/__init__.py
+++ octoprint_preheat/__init__.py
@@ -26,8 +26,28 @@ class PreheatAPIPlugin(octoprint.plugin.TemplatePlugin,
 		return dict(
 			preheat = []
 		)
+		
+	def parse_line(self, line):
+		line = strip_comment(line)
+		
+		tool = "tool0"
+		temperature = None
+		for item in line.split(" "):
+			if item.startswith("S"):
+				try:
+					value = float(item[1:])
+					if value > 0:
+						temperature = value
+				except ValueError:
+					self._logger.warn("Error parsing heat command: {}".format(line))
+					pass
+			if item.startswith("T"):
+				tool = "tool" + item[1:].strip()
+				
+		return tool, temperature
+		
 
-	def get_print_temperature(self):
+	def get_temperatures(self):
 		printer = self._printer
 		
 		if (printer.get_current_job()["file"]["path"] == None):
@@ -39,33 +59,38 @@ class PreheatAPIPlugin(octoprint.plugin.TemplatePlugin,
 		file = open(path_on_disk, 'r')
 		line = file.readline()
 		max_lines = 1000
+		temperatures = dict()
 		try:
 			with open(path_on_disk, "r") as file:
 				while max_lines > 0:
 					line = file.readline()
 					if line == "":
 						break
-					if line.startswith("M104 S") or line.startswith("M109 S"):
-						line = strip_comment(line)
-						try:
-							value = float(line[6:])
-							if value > 0:
-								return value
-						except ValueError:
-							self._logger.warn("Error parsing heat command: {}".format(line))
-							pass
+					if line.startswith("M104") or line.startswith("M109"):	# Set tool temperature
+						tool, temperature = self.parse_line(line)
+						if temperature != None and tool not in temperatures:
+							temperatures[tool] = temperature
+					if line.startswith("M190") or line.startswith("M140"):	# Set bed temperature
+						_, temperature = self.parse_line(line)
+						if temperature != None and "bed" not in temperatures:
+							temperatures["bed"] = temperature
+						
 					max_lines -= 1
 		except:
 			self._logger.exception("Something went wrong while trying to read the preheat temperature from {}".format(path_on_disk))
-
-		raise PreheatError("Could not find a preheat command in the gcode file.")
+		
+		if len(temperatures) == 0:
+			raise PreheatError("Could not find a preheat command in the gcode file.")
+		return temperatures
 		
 	def preheat(self):
 		if not self._printer.is_operational() or self._printer.is_printing():
 			raise PreheatError("Can't set the temperature because the printer is not ready.")
-		new_target = self.get_print_temperature()
-		self._logger.info("Print temp: " + str(new_target))
-		self._printer.set_temperature("tool0", new_target)
+		
+		temperatures = self.get_temperatures()
+		for key in temperatures:
+			self._logger.info("Preheating " + key + " to " + str(temperatures[key]))
+			self._printer.set_temperature(key, temperatures[key])
 	
 	def on_api_command(self, command, data):
 		import flask
